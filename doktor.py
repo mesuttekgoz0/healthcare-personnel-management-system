945c24f192d1a6e92395862b6b65e4c13ce127f7
Mesut <mesuttekgoz0@gmail.com>
2024-06-03 01:53:12 +0300

    .

--- doktor.py
+++ doktor.py
@@ -1,4 +1,27 @@
-class Doktor():
-    def __init__(self):
-        pass
+import personel
+class Doktor(personel.Personel):
+    def __init__(self, ad, soyad, departman, maaş, uzmanlik, deneyim_yili, hastane):
+        super().__init__(ad, soyad, departman, maaş)
+        self.__uzmanlik=uzmanlik
+        self.__deneyim_yili=deneyim_yili
+        self.__hastane=hastane
+
+    def get_uzmanlik(self):
+        return self.__uzmanlik
+    
+    def get_deneyim_yili(self):
+        return self.__deneyim_yili
+    
+    def get_hastane(self):
+        return self.__hastane
+    
+    def maas_arttir(self):
+        kat=1.1
+        yeni_maas=self.__maaş*kat
+        self.__maaş=yeni_maas
+        return self.__maaş
+    
+    def __str__(self):
+        return f"{super().__str__()} {self.__deneyim_yili} {self.__hastane} {self.__maaş}"
+        
         
---
+++ hemşire.py
@@ -0,0 +1,24 @@
+import personel
+class Hemsire(personel.Personel):
+    def __init__(self, ad, soyad, departman, maaş, caalisma_saati, sertifika, hastane):
+        super().__init__(ad, soyad, departman, maaş)
+        self.__calisma_saati=caalisma_saati
+        self.__sertifika=sertifika
+        self.__hastane=hastane
+    def get_calisma_saati(self):
+        return self.__calisma_saati
+    
+    def get_sertifika(self):
+        return self.__sertifika
+    
+    def get_hastane(self):
+        return self.__hastane
+    
+    def maas_arttir(self):
+        kat=1.05
+        yeni_maas=self.__maaş*kat
+        self.__maaş=yeni_maas
+        return self.__maaş
+    
+    def __str__(self):
+        return f"{super().__str__()} {self.__calisma_saati} {self.__sertifika} {self.__hastane}"
